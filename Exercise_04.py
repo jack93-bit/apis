ce640abe0d537975d7180fdf1bd4a571fcfff190
Jacqueline <jalvesdasilv@gmail.com>
2025-03-08 06:08:42 -0300

    exercicios 4 e extra

--- Exercise_04.py
+++ Exercise_04.py
@@ -4,3 +4,14 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+import requests
+from pprint import pprint
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users/10470"
+dados = {"email":"jkas@gmail.com", 
+         "first_name":"Jacqueline",
+         "last_name":"Silva"}
+
+requests.put(base_url, json=dados)
+
+pprint(requests.get(base_url).json())
